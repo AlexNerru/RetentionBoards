4a6200b3b120aa0a3a6321419670d2d8df8a3532
Alexander Karpin <Karpin_Aleksandr_1999@mail.ru>
2020-04-12 16:56:31 +0300

    docs added, structure changed

--- code/retentionboards_core/app/retentionboards_core/tasks.py
+++ code/retentionboards_core/app/retentionboards_core/tasks.py
@@ -5,6 +5,7 @@ from time import sleep
 from celery_manager import app
 
 
+
 '''@app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(60.0, send_ping.s('hello'), name='ping', options={'routing_key': 'task'})
